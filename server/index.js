97fd5cab4a5ed3f2632e687d1317f09070916a1f
Kira Porter <kira@dubsado.com>
2022-03-31 15:35:02 -0700

    Achieve socket.io connection

--- server/index.js
+++ server/index.js
@@ -2,19 +2,36 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
-const socketio = require("socket.io");
-const io = new socketio(server);
-
-app.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
-const PORT = process.env.PORT || 3000;
-
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+});
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
 });
 
+io.on("connect_error", (err) => {
+  console.log(`connect_error due to ${err.message}`);
+});
+
 io.on("connection", (socket) => {
-  socket.on("message", (msg) => {});
+  console.log("a user connected");
+  socket.join("chat");
+  socket.on("message sent", (message) => {
+    // to all connected clients in the "news" room
+    io.to("chat").emit(message);
+  });
+  socket.on("test", (msg) => console.log(msg));
+  // socket.emit();
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
